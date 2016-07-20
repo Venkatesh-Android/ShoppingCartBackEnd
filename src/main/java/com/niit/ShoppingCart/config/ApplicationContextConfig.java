e4959abf91dfdf80b07f9f8a3f84c4a9858dde9d
Venkatesh-Android <venkateshprabhakaran@gmail.com>
2016-07-21 01:17:31 +0530

    In BackEnd SLT_Tuesday(19/7/16) work
    Just Added ApplicationContextCofig java file and linked with hibernate.
    Then category java file along with its DAO interface & Implements and
    category_Test-class file

---
+++ src/main/java/com/niit/ShoppingCart/config/ApplicationContextConfig.java
@@ -0,0 +1,85 @@
+package com.niit.ShoppingCart.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.niit.ShoppingCart.dao.CategoryDAO;
+import com.niit.ShoppingCart.dao.CategoryDAOImpl;
+import com.niit.ShoppingCart.model.Category;
+
+@Configuration
+@ComponentScan("com.niit.ShoppingCart")
+@EnableTransactionManagement
+
+
+public class ApplicationContextConfig {
+	
+
+	
+ 	
+	@Bean(name = "dataSource")
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+
+		dataSource.setUsername("sa");
+		dataSource.setPassword("password");
+		return dataSource;
+	}
+    
+   
+    
+    private Properties getHibernateProperties() {
+    	Properties properties = new Properties();
+    	properties.put("hibernate.show_sql", "true");
+     	properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+    	return properties;
+    }
+    
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) {
+    	LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+    	sessionBuilder.addProperties(getHibernateProperties());
+    	sessionBuilder.addAnnotatedClass(Category.class);
+      	return sessionBuilder.buildSessionFactory();
+    }
+    
+	@Autowired
+	@Bean(name = "transactionManager")
+	public HibernateTransactionManager getTransactionManager(
+			SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(
+				sessionFactory);
+
+		return transactionManager;
+	}
+    
+   
+
+
+	
+	
+	  @Autowired
+	    @Bean(name = "categoryDao")
+	    public CategoryDAO getCategoryDao(SessionFactory sessionFactory) {
+	    	return new CategoryDAOImpl(sessionFactory);
+	    }
+	
+	
+	
+	
+
+}
